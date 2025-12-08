7742832a0a10a36f2f9130bab21d94e960ec723c
Fedecrive <crive.fede02@gmail.com>
2025-12-08 12:44:28 +0100

    sistemato

--- cvar_obj.m
+++
@@ -1,6 +0,0 @@
-function cvar_val = cvar_obj(w, LogRet, alpha)
-    pRet = LogRet * w;             % scenario returns
-    VaR  = quantile(pRet, 1-alpha);
-    tail = pRet(pRet <= VaR);
-    cvar_val = -mean(tail);        % minimize losses in the 5% worst cases
-end
